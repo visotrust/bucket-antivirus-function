829b007a6a511f0b6db2661a09039e641560eed1
dan h. <daniel@visotrust.com>
2024-05-28 15:17:32 -0600

    temporarily disable deploy to av update and prod/demo for testing purposes

--- scripts/build_and_deploy_lambda.sh
+++ scripts/build_and_deploy_lambda.sh
@@ -18,7 +18,7 @@ export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq -r .Credentials.SessionToken)
 
-aws lambda update-function-code --function-name bucket-antivirus-update --zip-file fileb://build/lambda.zip
+# aws lambda update-function-code --function-name bucket-antivirus-update --zip-file fileb://build/lambda.zip
 aws lambda update-function-code --function-name dev-bucket-antivirus-scan --zip-file fileb://build/lambda.zip
-aws lambda update-function-code --function-name prod-bucket-antivirus-scan --zip-file fileb://build/lambda.zip
+# aws lambda update-function-code --function-name prod-bucket-antivirus-scan --zip-file fileb://build/lambda.zip
 # aws lambda update-function-code --function-name demo-bucket-antivirus-scan --zip-file fileb://build/lambda.zip
